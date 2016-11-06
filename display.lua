8365a097c52ad63f7a87d7a2205978b9a8bdd857
Thomas R. Koll <tomk32@tomk32.de>
2016-11-06 13:53:02 +0100

    Add display.message

--- display.lua
+++ display.lua
@@ -1,7 +1,8 @@
 local disp
+local module = {}
 
 -- setup I2c and connect display
-function setup()
+function module.setup()
     -- SDA and SCL can be assigned freely to available GPIOs
     local sda = 2
     local scl = 1
@@ -10,17 +11,28 @@ function setup()
     disp = u8g.ssd1306_128x64_i2c(sla)
 end 
 
-function draw(super_mario_count, timer_id)
+function module.message(str)
     disp:firstPage()
     repeat
-      disp:setFont(u8g.font_fub20r)
       disp:setFontPosTop()
-      disp:drawStr(0, 0, "Super Mario Count")
+      disp:drawStr(0, 20, str)
+    until disp:nextPage() == false
+end
+
+function module.draw(super_mario_count, timer_id)
+    disp:firstPage()
+    repeat
+      disp:setFont(u8g.font_7x14Br)
+      disp:setFontPosTop()
+      disp:drawStr(4, 0, "Super Mario Count")
 
       disp:setFont(u8g.font_freedoomr25n)
       disp:drawStr(50, 60, super_mario_count)
     until disp:nextPage() == false
     tmr.start(timer_id)
 end
+function module.display()
+    return disp
+end
 
-return { setup: setup, draw: draw }
+return module
