5ffa557aa4453d3601873d001458b7a776b33839
Thomas R. Koll <tomk32@tomk32.de>
2016-11-06 13:55:16 +0100

    Fix counter

--- supermariocount.lua
+++ supermariocount.lua
@@ -4,31 +4,40 @@ super_mario_count = 0
 
 disp = require("display")
 disp.setup()
-local timer_id = 0
+timer_id = 3
 
-tmr.register(timer_id, 100, tmr.ALARM_SEMI, function() disp.draw(super_mario_count, timer_id) end)
+function draw_display()
+    disp.draw(super_mario_count, timer_id)
+end
+
+tmr.register(timer_id, 100, tmr.ALARM_SEMI, draw_display)
 tmr.start(timer_id)
 
 function change_super_mario_count(what)
-    super_maro_count = super_mario_count + what
-    // trigger mqtt calls
+    super_mario_count = super_mario_count + what
+    -- trigger mqtt calls
 end
+
 
 BTN_INC = 5
 BTN_DEC = 6
 
 gpio.mode(BTN_INC, gpio.INT)
 function increment(level)
-  change_super_mario_count(1)
-  // basically loop it
+   if(level == gpio.LOW) then
+      change_super_mario_count(1)
+   end
+  -- basically loop it
   gpio.trig(BTN_INC, level == gpio.HIGH and "down" or "up")
 end
 gpio.trig(BTN_INC, "down", increment)
 
 gpio.mode(BTN_DEC, gpio.INT)
 function decrement(level)
-  change_super_mario_count(-1)
-  // basically loop it
+   if(level == gpio.LOW) then
+      change_super_mario_count(-1)
+   end
+  -- basically loop it
   gpio.trig(BTN_DEC, level == gpio.HIGH and "down" or "up")
 end
 gpio.trig(BTN_DEC, "down", decrement)
