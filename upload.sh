2566b0e49201d4abbef2c9215dbf7a9ebe735aa2
Thomas R. Koll <tomk32@tomk32.de>
2016-11-29 12:59:50 +0100

    Exit upload scrip when one fails and upload init.lua uncompiled

--- upload.sh
+++ upload.sh
@@ -1,6 +1,12 @@
-files=(config.lua display.lua mqtt-client.lua wifi-devlol.lua tools.lua supermariocount.lua init.lua)
+files=(config.lua display.lua mqtt-client.lua wifi-devlol.lua tools.lua supermariocount.lua)
 
 for file in ${files[@]}; do
   echo $file
   luatool.py --port $1 --src $file --dest $file -c
+  if [ $? -ne 0 ]
+  then
+    exit 0
+  fi
 done
+file="init.lua"
+luatool.py --port $1 --src $file --dest $file
