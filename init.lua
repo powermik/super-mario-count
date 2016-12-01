5d6f569e04f7b3860828d4ca38078c5772ad0fcb
Thomas R. Koll <tomk32@tomk32.de>
2016-12-01 15:10:34 +0100

    No more global variable for the wifi. Conflicted with uploading from esplorer

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 config = require("config")  
-w = require("wifi-devlol")
 
 function load_super_mario_count()
     require("supermariocount")
 end
 
-w.start(load_super_mario_count)  
+require("wifi-devlol").start(load_super_mario_count)
+
