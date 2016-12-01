a7a90083bfeb4239a8a8ea2d9a803e4d894fc99c
Thomas R. Koll <tomk32@tomk32.de>
2016-12-01 18:40:19 +0100

    No global options in mqtt-client, last fixes

--- mqtt-client.lua
+++ mqtt-client.lua
@@ -5,17 +5,16 @@ local client = nil
 local options = nil
 
 function module.connect(options, connected)
-
   client:connect(options.host, options.port, options.secure, connected, 1,
                  function(c, reason) print("failed reason: "..reason) end)
 end
 
 function module.setup(opts, connected)
-    options = opts
+    module.options = opts
 
-    client = mqtt.Client(options.id, 120, options.user, options.password)
+    client = mqtt.Client(module.options.id, 120, module.options.user, module.options.password)
     client:on("connect", connected)
-    module.connect(options, connected)
+    module.connect(module.options, connected)
     return module
 end
 
@@ -24,15 +23,15 @@ function module.client()
 end
 
 function module.message(topic, message, retain)
-    client:publish(options.endpoint .. topic, message, 0, retain)
+    client:publish(module.options.endpoint .. topic, message, 0, retain)
 end
 
 function module.read_topic(topic, callback)
-    topic = options.endpoint .. topic
+    local topic = module.options.endpoint .. topic
     print("subscribing to " .. topic)
     client:on('message', function(c, t, message)
         if t == topic then
-            client:unsubscribe(topic)
+            client:unsubscribe(topic, function() print("unsubscribed " .. topic) end)
             callback(t, message)
         end
     end)
