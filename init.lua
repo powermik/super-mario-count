05f182b774e3e567a88c03d5555c450ac3501eeb
Thomas R. Koll <tomk32@tomk32.de>
2016-12-01 18:27:39 +0100

    Put supermariocount into a pcall to prevent any silly reboot orgies

--- init.lua
+++ init.lua
@@ -1,7 +1,13 @@
 config = require("config")  
 
 function load_super_mario_count()
-    require("supermariocount")
+
+    local status, err = pcall(function ()
+        require("supermariocount")
+    end)
+    if err then
+        print(err)
+    end
 end
 
 require("wifi-devlol").start(load_super_mario_count)
